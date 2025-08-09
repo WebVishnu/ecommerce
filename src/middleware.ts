1f91ba5fb843f5a05846068062f3e23ba930eaf2
Vishnu Goswami <87561982+WebVishnu@users.noreply.github.com>
2025-08-09 15:13:40 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
-  // this is goodasdfadasdfasdfasefasdfasdfad
+  // this is goodasdfadasdfasdfasefasdfasdfadsadfads
   
   return NextResponse.next();
 }
